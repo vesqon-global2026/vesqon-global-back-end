35be24d5de7314a95387c6ed841dc023da8b375a
ASWINI07-K <aswini710bca@gmail.com>
2026-03-25 00:58:07 +0300

    final backend fix

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ dotenv.config();
 
 const app = express();
 const upload = multer({ dest: "uploads/" });
-app.use(cors());
+app.use(cors({
+  origin: ["https://vesqon.com", "https://www.vesqon.com"],
+}));
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -138,6 +140,9 @@ ${message}
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 app.listen(process.env.PORT || 5000, () =>
   console.log(`Server running on port ${process.env.PORT || 5000}`)
 );
